0e3b3a1118fe336ec9cc9e39458454e7355d9881
Daniel Nephin <dnephin@gmail.com>
2023-01-13 16:02:02 -0500

    Removing binding types we don't need
    We only use JSON, URI, and Query.
    
    We don't use header binding, but keeping it for now.

--- binding.go
+++ binding.go
@@ -2,77 +2,13 @@
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file.
 
-//go:build !nomsgpack
-// +build !nomsgpack
-
 package binding
-
-import "net/http"
-
-// Content-Type MIME of the most common data formats.
-const (
-	MIMEJSON              = "application/json"
-	MIMEHTML              = "text/html"
-	MIMEXML               = "application/xml"
-	MIMEXML2              = "text/xml"
-	MIMEPlain             = "text/plain"
-	MIMEPOSTForm          = "application/x-www-form-urlencoded"
-	MIMEMultipartPOSTForm = "multipart/form-data"
-	MIMEPROTOBUF          = "application/x-protobuf"
-	MIMEMSGPACK           = "application/x-msgpack"
-	MIMEMSGPACK2          = "application/msgpack"
-	MIMEYAML              = "application/x-yaml"
-	MIMETOML              = "application/toml"
-)
-
-// Binding describes the interface which needs to be implemented for binding the
-// data present in the request such as JSON request body, query parameters or
-// the form POST.
-type Binding interface {
-	Name() string
-	Bind(*http.Request, any) error
-}
 
 // These implement the Binding interface and can be used to bind the data
 // present in the request to struct instances.
 var (
-	JSON          = jsonBinding{}
-	XML           = xmlBinding{}
-	Form          = formBinding{}
-	Query         = queryBinding{}
-	FormPost      = formPostBinding{}
-	FormMultipart = formMultipartBinding{}
-	ProtoBuf      = protobufBinding{}
-	MsgPack       = msgpackBinding{}
-	YAML          = yamlBinding{}
-	Uri           = uriBinding{}
-	Header        = headerBinding{}
-	TOML          = tomlBinding{}
+	JSON   = jsonBinding{}
+	Query  = queryBinding{}
+	Uri    = uriBinding{}
+	Header = headerBinding{}
 )
-
-// Default returns the appropriate Binding instance based on the HTTP method
-// and the content type.
-func Default(method, contentType string) Binding {
-	if method == http.MethodGet {
-		return Form
-	}
-
-	switch contentType {
-	case MIMEJSON:
-		return JSON
-	case MIMEXML, MIMEXML2:
-		return XML
-	case MIMEPROTOBUF:
-		return ProtoBuf
-	case MIMEMSGPACK, MIMEMSGPACK2:
-		return MsgPack
-	case MIMEYAML:
-		return YAML
-	case MIMETOML:
-		return TOML
-	case MIMEMultipartPOSTForm:
-		return FormMultipart
-	default: // case MIMEPOSTForm:
-		return Form
-	}
-}
